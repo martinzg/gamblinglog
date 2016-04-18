c62822aa2c745617ff053aff5fedf1f60a9f7a8a
tyoma17 <tyomashootoff@gmail.com>
2016-04-18 06:41:58 +0300

    added stakes controller

---
+++ src/main/java/lv/javaguru/java2/servlet/mvc/StakesController.java
@@ -0,0 +1,39 @@
+package lv.javaguru.java2.servlet.mvc;
+
+import lv.javaguru.java2.database.DBException;
+import lv.javaguru.java2.database.StakeDAO;
+import lv.javaguru.java2.database.jdbc.StakeDAOImpl;
+import lv.javaguru.java2.domain.Stake;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+/**
+ * Created by tyoma17 on 18.04.2016.
+ */
+public class StakesController implements MVCController {
+
+    @Override
+    public MVCModel processRequestGet(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        Long id = Long.parseLong(session.getAttribute("userID").toString());
+        StakeDAO stakeDAO = new StakeDAOImpl();
+
+        try {
+            List<Stake> stakeList = stakeDAO.getAllStakes(id);
+            return new MVCModel("/Stakes.jsp", stakeList, null);
+        } catch (DBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Override
+    public MVCModel processRequestPost(HttpServletRequest request) {
+        if (request.getParameter("add stake") != null) {
+            return new MVCModel("/Redirect.jsp", "stake-add", null);
+        } else {
+            return new MVCModel("/Stakes.jsp", null, null);
+        }
+    }
+}
