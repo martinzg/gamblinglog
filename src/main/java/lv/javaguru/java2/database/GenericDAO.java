a188b5bac3c53e46c919261dca4dae2bf8de6845
martinzg <martinzg@inbox.lv>
2016-04-19 19:59:17 +0300

    Lesson 5 - added GenericDao for Hibernate

---
+++ src/main/java/lv/javaguru/java2/database/GenericDAO.java
@@ -0,0 +1,21 @@
+package lv.javaguru.java2.database;
+
+import org.hibernate.JDBCException;
+
+import java.util.List;
+
+public interface GenericDAO<T> {
+    void create(T obj) throws JDBCException;
+
+    void update(T obj) throws JDBCException;
+
+    List<T> getAll() throws JDBCException;
+
+    T getById(long id) throws JDBCException;
+
+    void delete(long id) throws JDBCException;
+
+    T getByObjectName(String objectName) throws JDBCException;
+
+    boolean isEmpty() throws JDBCException;
+}
