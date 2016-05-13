6e42bb192b6b5ed75cca5d2997ded7242eeaccb3
martinzg <martinzg@inbox.lv>
2016-05-13 21:56:12 +0300

    Lesson 7 - fixed UserDAOImpl test

--- src/test/test/lv/javaguru/java2/database/jdbc/UserDAOImplTest.java
+++ src/test/test/lv/javaguru/java2/database/jdbc/UserDAOImplTest.java
@@ -15,10 +15,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import test.lv.javaguru.java2.servlet.mvc.UserCreator;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SpringAppConfig.class)
+@WebAppConfiguration
 
 public class UserDAOImplTest {
 
